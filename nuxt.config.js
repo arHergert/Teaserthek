ab26395eb3f9a7ed90a3731385d1e0a1175d0c86
Artur Hergert <hergert.artur@web.de>
2021-04-02 14:36:51 +0200

    Removed unused PWA entries

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,9 +43,7 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
